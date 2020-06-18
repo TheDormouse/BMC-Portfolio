1b47e87e244556c383e1905ef59ca60097b35fb6
Ben Martinez-Cain <bmc@bens-air.lan>
2020-06-18 16:02:40 -0400

    analytics change

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import ReactGA from 'react-ga';
-
+import { Analytics } from '../lib/analytics'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
-    ReactGA.initialize('UA-169904775-1');
-    ReactGA.pageview(ctx.asPath);
+    Analytics.logPageView(ctx.asPath)
     return { ...initialProps }
   }
   
